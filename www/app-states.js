7ac93e4aaee1103dac8a11b5c8b376568966519e
jianyuan <jiyuan12354@hotmail.com>
2016-11-08 11:36:40 +0800

    update

--- www/app-states.js
+++ www/app-states.js
@@ -3,7 +3,7 @@
  */
 'use strict';
 
-    var app = angular.module('myApp',['ui.router','home.controllers','detail.controller']);
+    var app = angular.module('myApp',['ui.router','home.controllers','detail.controller','ngTouch']);
 
     app.config(function($stateProvider, $urlRouterProvider) {
         $stateProvider.state('home', {
@@ -16,3 +16,182 @@
         });
         $urlRouterProvider.otherwise('/home');
     });
+/*
+var IonicModule = angular.module('ionic', ['ngAnimate', 'ngSanitize', 'ui.router']),
+    extend = angular.extend,
+    forEach = angular.forEach,
+    isDefined = angular.isDefined,
+    isNumber = angular.isNumber,
+    isString = angular.isString,
+    jqLite = angular.element,
+    noop = angular.noop;
+    IonicModule.directive('ionList', ['$timeout', function($timeout){
+        return {
+            restrict: 'E',
+            require: ['ionList', '^?$ionicScroll'],
+            controller: '$ionicList',
+            compile: function($element, $attr) {
+                var listEl = jqLite('<div class="list">')
+                    .append($element.contents())
+                    .addClass($attr.type);
+
+                $element.append(listEl);
+
+                return function($scope, $element, $attrs, ctrls) {
+                    var listCtrl = ctrls[0];
+                    var scrollCtrl = ctrls[1];
+
+                    // Wait for child elements to render...
+                    $timeout(init);
+
+                    function init() {
+                        var listView = listCtrl.listView = new ionic.views.ListView({
+                            el: $element[0],
+                            listEl: $element.children()[0],
+                            scrollEl: scrollCtrl && scrollCtrl.element,
+                            scrollView: scrollCtrl && scrollCtrl.scrollView,
+                            onReorder: function(el, oldIndex, newIndex) {
+                                var itemScope = jqLite(el).scope();
+                                if (itemScope && itemScope.$onReorder) {
+                                    // Make sure onReorder is called in apply cycle,
+                                    // but also make sure it has no conflicts by doing
+                                    // $evalAsync
+                                    $timeout(function() {
+                                        itemScope.$onReorder(oldIndex, newIndex);
+                                    });
+                                }
+                            },
+                            canSwipe: function() {
+                                return listCtrl.canSwipeItems();
+                            }
+                        });
+
+                        $scope.$on('$destroy', function() {
+                            if (listView) {
+                                listView.deregister && listView.deregister();
+                                listView = null;
+                            }
+                        });
+
+                        if (isDefined($attr.canSwipe)) {
+                            $scope.$watch('!!(' + $attr.canSwipe + ')', function(value) {
+                                listCtrl.canSwipeItems(value);
+                            });
+                        }
+                        if (isDefined($attr.showDelete)) {
+                            $scope.$watch('!!(' + $attr.showDelete + ')', function(value) {
+                                listCtrl.showDelete(value);
+                            });
+                        }
+                        if (isDefined($attr.showReorder)) {
+                            $scope.$watch('!!(' + $attr.showReorder + ')', function(value) {
+                                listCtrl.showReorder(value);
+                            });
+                        }
+
+                        $scope.$watch(function() {
+                            return listCtrl.showDelete();
+                        }, function(isShown, wasShown) {
+                            //Only use isShown=false if it was already shown
+                            if (!isShown && !wasShown) { return; }
+
+                            if (isShown) listCtrl.closeOptionButtons();
+                            listCtrl.canSwipeItems(!isShown);
+
+                            $element.children().toggleClass('list-left-editing', isShown);
+                            $element.toggleClass('disable-pointer-events', isShown);
+
+                            var deleteButton = jqLite($element[0].getElementsByClassName('item-delete'));
+                            setButtonShown(deleteButton, listCtrl.showDelete);
+                        });
+
+                        $scope.$watch(function() {
+                            return listCtrl.showReorder();
+                        }, function(isShown, wasShown) {
+                            //Only use isShown=false if it was already shown
+                            if (!isShown && !wasShown) { return; }
+
+                            if (isShown) listCtrl.closeOptionButtons();
+                            listCtrl.canSwipeItems(!isShown);
+
+                            $element.children().toggleClass('list-right-editing', isShown);
+                            $element.toggleClass('disable-pointer-events', isShown);
+
+                            var reorderButton = jqLite($element[0].getElementsByClassName('item-reorder'));
+                            setButtonShown(reorderButton, listCtrl.showReorder);
+                        });
+
+                        function setButtonShown(el, shown) {
+                            shown() && el.addClass('visible') || el.removeClass('active');
+                            ionic.requestAnimationFrame(function() {
+                                shown() && el.addClass('active') || el.removeClass('visible');
+                            });
+                        }
+                    }
+
+                };
+            }
+        };
+    }]);
+
+    IonicModule.directive('ionItem', [function() {
+    return {
+        restrict: 'E',
+        controller: ['$scope', '$element', function($scope, $element) {
+            this.$scope = $scope;
+            this.$element = $element;
+        }],
+        scope: true,
+        compile: function($element, $attrs) {
+            var isAnchor = isDefined($attrs.href) ||
+                isDefined($attrs.ngHref) ||
+                isDefined($attrs.uiSref);
+            var isComplexItem = isAnchor ||
+                    //Lame way of testing, but we have to know at compile what to do with the element
+                /ion-(delete|option|reorder)-button/i.test($element.html());
+
+            if (isComplexItem) {
+                var innerElement = jqLite(isAnchor ? '<a></a>' : '<div></div>');
+                innerElement.addClass('item-content');
+
+                if (isDefined($attrs.href) || isDefined($attrs.ngHref)) {
+                    innerElement.attr('ng-href', '{{$href()}}');
+                    if (isDefined($attrs.target)) {
+                        innerElement.attr('target', '{{$target()}}');
+                    }
+                }
+
+                innerElement.append($element.contents());
+
+                $element.addClass('item item-complex')
+                    .append(innerElement);
+            } else {
+                $element.addClass('item');
+            }
+
+            return function link($scope, $element, $attrs) {
+                $scope.$href = function() {
+                    return $attrs.href || $attrs.ngHref;
+                };
+                $scope.$target = function() {
+                    return $attrs.target;
+                };
+
+                var content = $element[0].querySelector('.item-content');
+                if (content) {
+                    $scope.$on('$collectionRepeatLeave', function() {
+                        if (content && content.$$ionicOptionsOpen) {
+                            content.style[ionic.CSS.TRANSFORM] = '';
+                            content.style[ionic.CSS.TRANSITION] = 'none';
+                            $$rAF(function() {
+                                content.style[ionic.CSS.TRANSITION] = '';
+                            });
+                            content.$$ionicOptionsOpen = false;
+                        }
+                    });
+                }
+            };
+
+        }
+    };
+}]);*/
--- www/module/home/homeControler.js
+++ www/module/home/homeControler.js
@@ -24,10 +24,135 @@ angular.module('fsCordova', [])
     ]);
 
 angular.module('home.controllers', ['fsCordova'])
+    .directive("messageItem",['$state','$rootScope',function($state,$rootScope){
+        return{
+            restrict:"AE",
+            scope:false,
+            scope:{
+                message:'=item',
+            },
+            templateUrl:'module/template/messageItem.html',
+            replace:true,
+            link:function(scope,element,attrs){
+                /*element.on('dblclick',function(){
+                 element.addClass('selected');
+                 //element.addClass('selected').siblings().removeClass('selected');
+                 });
+                 element.on('click',function(){
+                 element.stop().animate(
+                 {},100,function(){
+                 element.removeClass('selected');
+                 }
+                 )
+                 });*/
+                scope.checkFlag = false;
+                scope.showDeleteBtn = function(id)
+                {
+                    scope.$root.choosedId = id;
+                    //element.addClass('selected');
+                }
+                scope.hideDeleteBtn = function()
+                {
+                    scope.$root.choosedId = -1;
+                    //element.removeClass('selected');
+                }
+                scope.stategoDetail = function (message) {
+                    $state.go('message-detail',{Content:message.messageContent,PushDate:message.pushDate})
+                    var db = window.sqlitePlugin.openDatabase({name:"JpushMessageDB.db",location:1});
+                    db.transaction(function(tx){
+                        var sql1 = "update jpush_message set readflag = 1 where id="+message.id;
+                        tx.executeSql(sql1,[], function (tx, res) {
+                            scope.$parent.$parent.$apply(function()
+                            {
+                                var index = scope.$parent.$parent.messages.indexOf(message);
+                                scope.$parent.$parent.messages.splice(index,1);
+                            });
+
+                        });
+                    });
+                }
+                scope.delete = function(message)
+                {
+                    var db = window.sqlitePlugin.openDatabase({name:"JpushMessageDB.db",location:1});
+                    db.transaction(function(tx){
+                        var sql1 = "delete from jpush_message where id="+message.id;
+                        tx.executeSql(sql1,[], function (tx, res) {
+                            scope.$parent.$parent.$apply(function()
+                            {
+                                var index = scope.$parent.$parent.messages.indexOf(message);
+                                scope.$parent.$parent.messages.splice(index,1);
+                            });
+
+                        });
+                    });
+                }
+                scope.checkItem = function(id)
+                {
+                    scope.checkFlag = !scope.checkFlag;
+                    scope.$root.choosedEditIds.push(id);
+                }
+                scope.uncheckItem = function(id)
+                {
+                    scope.checkFlag = !scope.checkFlag;
+                    var index = scope.$root.choosedEditIds.indexOf(id);
+                    scope.$root.choosedEditIds.splice(index,1);
+                }
+            }
+        }
+    }])
     .controller('homeController',
-        ['$scope', '$state','$timeout','CordovaService',function($scope, $state,$timeout,CordovaService) {
+        ['$scope','$rootScope','$state','$timeout','CordovaService',function($scope,$rootScope,$state,$timeout,CordovaService) {
+            $rootScope.choosedId = -1; //用于记录当前侧滑事件发生在哪个消息上
+            $rootScope.showEditFlag = false; //显示编辑图标
+            $rootScope.selectAllFlag = false; //全选标志
+            $rootScope.choosedEditIds = []; //当前已选择编辑的消息列表
+            $scope.editOptionMessages = ['编辑','取消']; //用于点击编辑/取消编辑的切换显示
+            $scope.editMessage = '编辑'; //默认显示编辑，点击编辑之后显示取消
+            $scope.selectMessage = '全选'; //全选和取消全选切换，默认显示全选
+/*            $scope.messages = [
+                {
+                    id:1,
+                    readflag:1,
+                    messageContent:'good morning',
+                    pushDate:'2016.11.01'
+                },
+                {
+                    id:2,
+                    readflag:0,
+                    messageContent:'good afternoon',
+                    pushDate:'2016.11.01'
+                }
+            ];*/
+
+            //切换编辑文字，并显示编辑图片
+            $scope.showEdit = function () {
+                if($scope.editMessage==$scope.editOptionMessages[0])//点击编辑
+                {
+                    $rootScope.showEditFlag = true;
+                    $scope.editMessage = $scope.editOptionMessages[1];
+                }
+                else//点击取消
+                {
+                    $rootScope.choosedEditIds = [];
+                    $scope.editMessage = $scope.editOptionMessages[0];
+                    $rootScope.showEditFlag = false;
+                }
+            };
+
+            //切换全选文字，并改变标志位
+            $scope.selectAll = function () {
+                $rootScope.selectAllFlag = !$rootScope.selectAllFlag;
+                if(!$rootScope.selectAllFlag)
+                {
+                    $rootScope.choosedEditIds = [];
+                }
+                //切换全选，取消全选文字显示
+                $scope.selectMessage = $rootScope.selectAllFlag?'取消全选':"全选";
+            };
+
             CordovaService.ready.then(function () {
                 /*cordova已经准备好了*/
+
 
 
                 //从表jpush_message中遍历出过滤后的messages，返回到$scope.messages
@@ -56,9 +181,17 @@ angular.module('home.controllers', ['fsCordova'])
 
                             });
                         });
-
-
-                }
+                };
+                var reset = function() //用于重置scope的属性 刷新到最初状态
+                {
+                    $rootScope.choosedId = -1; //用于记录当前侧滑事件发生在哪个消息上
+                    $rootScope.showEditFlag = false; //显示编辑图标
+                    $rootScope.selectAllFlag = false; //全选标志
+                    $rootScope.choosedEditIds = []; //当前已选择编辑的消息列表
+                    $scope.editOptionMessages = ['编辑','取消']; //用于点击编辑/取消编辑的切换显示
+                    $scope.editMessage = '编辑'; //默认显示编辑，点击编辑之后显示取消
+                    $scope.selectMessage = '全选'; //全选和取消全选切换，默认显示全选
+                };
 
                 //监控事件’deviceready‘ 程序主要入口
                 var onDeviceReady = function() {
@@ -75,18 +208,10 @@ angular.module('home.controllers', ['fsCordova'])
                     $scope.messages = [
                     ];
 
-                    //重置搜索关键字
+/*                    //重置搜索关键字
                     $scope.reset = function () {
                         $scope.searchValue = '';
-                    };
-
-                    $scope.stategoDetail = function (item) {
-                        $state.go('message-detail',{PushDate:item.pushDate,Content:item.messageContent});
-                    };
-
-                    $scope.stategoDetail0 = function () {
-                        $state.go('message-detail',{});
-                    };
+                    };*/
 
                     //延迟搜索，监控searchValue输入变化
                     var timeout;
@@ -101,10 +226,67 @@ angular.module('home.controllers', ['fsCordova'])
                                     searchMessage(newValue);
                                 },500)
                             }
-                            $scope.$apply();
+                            //$scope.$apply();
                         }
                     });
 
+                    $scope.doDelete = function()
+                    {
+                        var db = window.sqlitePlugin.openDatabase({name:"JpushMessageDB.db",location:1});
+                        db.transaction(function(tx){
+                            var sql= '';
+                            if($rootScope.selectAllFlag||$rootScope.choosedEditIds.length==messages.length)
+                            {
+                                sql = "delete from jpush_message";
+                            }
+                            else
+                            {
+                                var iDstr = '(';//组装数组数据传递给sqlite
+                                angular.forEach($rootScope.choosedEditIds,function(data,index,array)
+                                {
+                                    iDstr = iDstr+array[index]+',';
+                                });
+                                iDstr = iDstr+"-1）"//组装完毕
+                                sql = "delete from jpush_message where id in"+iDstr;
+                            }
+                            tx.executeSql(sql,[], function (tx, res) {
+                                $scope.$apply(function()
+                                {
+                                    searchMessage(searchValue);
+                                });
+
+                            });
+                        });
+
+                    };
+                    $scope.setRead = function()
+                    {
+                        var db = window.sqlitePlugin.openDatabase({name:"JpushMessageDB.db",location:1});
+                        db.transaction(function(tx){
+                            var sql= '';
+                            if($rootScope.selectAllFlag)
+                            {
+                                sql = "update jpush_message set readflag = 1";
+                            }
+                            else
+                            {
+                                var iDstr = '(';//组装数组数据传递给sqlite
+                                angular.forEach($rootScope.choosedEditIds,function(data,index,array)
+                                {
+                                    iDstr = iDstr+array[index]+',';
+                                });
+                                iDstr = iDstr+"-1)"//组装完毕
+                                sql = "update jpush_message set readflag = 1 where id in "+iDstr;
+                            }
+                            tx.executeSql(sql,[], function (tx, res) {
+                                $scope.$apply(function()
+                                {
+                                    searchMessage(searchValue);
+                                });
+
+                            });
+                        });
+                    };
                     searchMessage($scope.searchValue);
                 };
 
@@ -170,39 +352,8 @@ angular.module('home.controllers', ['fsCordova'])
                     }
                 };
 
-                //var startX ,startY;
-                //document.addEventListener("touchstart", function(e) {
-                //    e.preventDefault();
-                //    startX = e.originalEvent.changedTouches[0].pageX;
-                //    startY = e.originalEvent.changedTouches[0].pageY;
-                //});
-                //
-                //document.addEventListener("touchmove", function(e) {
-                //    e.preventDefault();
-                //    var moveEndX = e.originalEvent.changedTouches[0].pageX,
-                //        moveEndY = e.originalEvent.changedTouches[0].pageY,
-                //        X = moveEndX - startX,
-                //        Y = moveEndY - startY;
-                //
-                //    if ( Math.abs(X) > Math.abs(Y) && X > 0 ) {
-                //        alert("left 2 right");
-                //    }
-                //    else if ( Math.abs(X) > Math.abs(Y) && X < 0 ) {
-                //        alert("right 2 left");
-                //    }
-                //    else if ( Math.abs(Y) > Math.abs(X) && Y > 0) {
-                //        alert("top 2 bottom");
-                //    }
-                //    else if ( Math.abs(Y) > Math.abs(X) && Y < 0 ) {
-                //        alert("bottom 2 top");
-                //    }
-                //    else{
-                //        alert("just touch");
-                //    }
-                //
-                //});
-
                 document.addEventListener("deviceready", onDeviceReady, false);
                 document.addEventListener("jpush.openNotification", onOpenNotification, false);
             });
         }]);
+
